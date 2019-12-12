52e2742f3b8cd534e695b8249c8ce96beb71c2d4
antilou86 <antilou86@gmail.com>
2019-12-11 19:12:34 -0500

    added comments to recipe_batches for readability.

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,16 +3,22 @@
 import math
 
 def recipe_batches(recipe, ingredients):
+  #initialize an empty array
   batches = []
+  #if you dont have enough ingredients, you fail. return zero
   if len(ingredients) < len(recipe):
     batches.append(0)
+  #if you have enough ingredients check each ingredient on hand against the recipe
   else:
     for i in ingredients:
       for r in recipe:
+        #if the values match up...
         if i == r:
+          #...see how many batches can be made
           x = ingredients[i] // recipe[r]
+          #push the number of single ingredient batches into the array.
           batches.append(x)
-
+  #sort the resulting array and return the lowest number of batches you can make.
   return sorted(batches)[0]
 
       
